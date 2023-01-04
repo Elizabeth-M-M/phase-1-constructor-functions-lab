311c2ad32fd41aaf81f40b5e70c1b768ac9cf9d9
Elizabeth <mwendemuthusi6@gmail.com>
2023-01-04 20:50:45 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function Scooter(year, color, model){
+  this.year = year
+  this.color = color
+  this.model = model
+}
+function Driver(name, age, experience){
+  this.name = name
+  this.age = age
+  this.experience = experience
+}
+function PickupLocation(address, city){
+  this.address = address
+  this.city = city 
+}
+let scoots = new Scooter('1994', 'red', 'B2543')
